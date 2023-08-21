24c99324081368cc0bcdb6b35294bae61990a7e9
DESKTOP-61QRH02\dungh <dunghasd@gmail.com>
2023-08-22 00:18:38 +0900

    backend insertUser add, duplicateEmailException add

---
+++ src/main/java/com/cmj/myproject/exception/DuplicateEmailException.java
@@ -0,0 +1,5 @@
+package com.cmj.myproject.exception;
+
+public class DuplicateEmailException extends RuntimeException{
+
+}
---
+++ src/main/java/com/cmj/myproject/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package com.cmj.myproject.repository;
+
+
+import com.cmj.myproject.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    boolean existsByEmail(String email);
+
+}
